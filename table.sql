c11f854e9292906df004c788f286e2239f965638
Josh Wilcox <Jwilcox@gap.com>
2020-09-08 16:22:35 -0600

    table creation statement

---
+++ table.sql
@@ -0,0 +1,14 @@
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[exchange_rate_USD](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[date_updated] [date] NULL,
+	[uts_updated] [int] NULL,
+	[currency] [varchar](5) NULL,
+	[rate] [decimal](12, 6) NULL
+) ON [PRIMARY]
+GO
