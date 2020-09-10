c48d42f436777ced54a08d03b8bf2d0fdb4f2e65
Josh Wilcox <Jwilcox@gap.com>
2020-09-10 08:54:15 -0600

    update data types

--- table.sql
+++ table.sql
@@ -5,7 +5,7 @@ SET QUOTED_IDENTIFIER ON
 GO
 
 CREATE TABLE [dbo].[exchange_rate_USD](
-	[id] [int] IDENTITY(1,1) NOT NULL,
+	[id] [int] IDENTITY(1,1),
 	[date_updated] [date] NULL,
 	[uts_updated] [int] NULL,
 	[currency] [varchar](5) NULL,
